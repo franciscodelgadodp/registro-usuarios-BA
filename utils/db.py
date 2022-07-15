6276d8a7b851f47105ea7049aeea5d708c8ca965
franciscodelgado <panch2203@gmail.com>
2022-07-14 21:16:58 -0500

    creando variables env para la uri de la BD

--- utils/db.py
+++ utils/db.py
@@ -1,8 +1,15 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-postgres_url = "postgresql://franciscodelgado_usuario_prueba:prueba_banco_198@postgresql-franciscodelgado.alwaysdata.net:5432/franciscodelgado_db_prueba"
+load_dotenv()
+
+database_name = os.getenv("DATABASE_NAME")
+database_pwd = os.getenv("DATABASE_PWD")
+
+postgres_url = f"postgresql://{database_name}:{database_pwd}@postgresql-franciscodelgado.alwaysdata.net:5432/franciscodelgado_db_prueba"
 
 engine = create_engine(postgres_url)
 
