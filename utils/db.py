6af84a8f943af618092326a332e8f0cbeeb367d0
franciscodelgadodp <franciscodelgadodp@gmail.com>
2022-07-18 15:04:28 -0500

    env

--- utils/db.py
+++ utils/db.py
@@ -1,8 +1,17 @@
+import os
+from traceback import print_tb
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-postgres_url = "postgresql://franciscodelgado_usuario_prueba:prueba_banco_198@postgresql-franciscodelgado.alwaysdata.net:5432/franciscodelgado_db_prueba"
+load_dotenv()
+
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+DATABASE_PWD = os.getenv("DATABASE_PWD")
+
+
+postgres_url = f"postgresql://{DATABASE_NAME}:{DATABASE_PWD}@postgresql-franciscodelgado.alwaysdata.net:5432/franciscodelgado_db_prueba"
 
 engine = create_engine(postgres_url)
 
